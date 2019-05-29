5fc322a2974e0e8a2c6caba0b25f262714f4bd2b
Jake.Zheng <Huibin.zheng@hpe.com>
2019-05-29 14:33:41 +0800

    up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require("gulp");
 const rename = require("gulp-rename");
-//const dest = "platform_web_server";
-const dest = "aaa";
+const dest = "platform_web_server";
+
 gulp.task('build', function() {
     // 将你的默认的任务代码放在这
     return gulp.src(["dist/*","!dist/index.html"])
