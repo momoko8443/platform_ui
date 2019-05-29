32f63de7b9c1e1c8a9c1ad22a0d7c9b4b83f86dc
Jake.Zheng <Huibin.zheng@hpe.com>
2019-05-29 15:48:17 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,8 @@ const dest = "platform_web_server";
 gulp.task('build', function() {
     // 将你的默认的任务代码放在这
     return gulp.src(["dist/*","!dist/index.html"])
-    .pipe(gulp.dest(`../${dest}/public`))
+    .pipe(gulp.dest(`../${dest}/public/`))
     .pipe(gulp.src("dist/index.html"))
     .pipe(rename("main.html"))
-    .pipe(gulp.dest(`../${dest}/views`));
+    .pipe(gulp.dest(`../${dest}/views/`));
   });
