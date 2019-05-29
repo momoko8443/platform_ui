e4a99226b89e545e8a6253f1332eb9e15420d775
Jake.Zheng <Huibin.zheng@hpe.com>
2019-05-29 16:14:11 +0800

    up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,15 @@ const gulp = require("gulp");
 const rename = require("gulp-rename");
 const dest = "platform_web_server";
 
-gulp.task('build', function() {
+
+gulp.task('deploy_static', function() {
     // 将你的默认的任务代码放在这
-    return gulp.src(["dist/*","!dist/index.html"])
-    .pipe(gulp.dest(`../${dest}/public/`))
-    .pipe(gulp.src("dist/index.html"))
-    .pipe(rename("main.html"))
-    .pipe(gulp.dest(`../${dest}/views/`));
-  });
+    return gulp.src(["./dist/*","!./dist/index.html"])
+    .pipe(gulp.dest(`../${dest}/public/`));
+});
+
+gulp.task('build',gulp.series('deploy_static', function(){
+    return gulp.src("./dist/index.html")
+    .pipe(rename("views/main.html"))
+    .pipe(gulp.dest(`../${dest}`));
+}));
