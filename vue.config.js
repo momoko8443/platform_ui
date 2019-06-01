bd7b39b2f53bf1777d6fdbc9eb75f6bedb04c554
Jake.Zheng <Huibin.zheng@hpe.com>
2019-06-01 17:30:12 +0800

    ignore zip html to gz

--- vue.config.js
+++ vue.config.js
@@ -39,7 +39,7 @@ module.exports = {
             new CompressionWebpackPlugin({
                 filename: '[path].gz[query]',
                 algorithm: 'gzip',
-                test: /\.js$|\.html$|\.json$|\.css/,
+                test: /\.js$|\.json$|\.css/,
                 threshold: 0, // 只有大小大于该值的资源会被处理
                 minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
                 deleteOriginalAssets: true // 删除原文件
